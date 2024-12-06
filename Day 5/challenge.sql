fdc9313d6707304f9ec02bedb6f351ebf4fe0dfd
Thiago de Assis <156924136+Mustasheep@users.noreply.github.com>
2024-12-06 14:24:24 -0300

    Delete Day 5/challenge.sql

--- Day 5/challenge.sql
+++
@@ -1,7 +0,0 @@
-SELECT polar_bears.bear_id, bear_name, SUM(distance_km) AS total_distance_traveled
-FROM polar_bears
-INNER JOIN tracking ON polar_bears.bear_id = tracking.bear_id
-WHERE date LIKE '2024-12%'
-GROUP BY polar_bears.bear_id
-ORDER BY total_distance_traveled DESC
-LIMIT 3;
