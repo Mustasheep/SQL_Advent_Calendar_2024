ca71c84f009b41cade56395679e0d40a5626a9f4
Thiago de Assis <156924136+Mustasheep@users.noreply.github.com>
2024-12-09 20:25:38 -0300

    Desafio de torrar os neurônios!

--- Calendar/Day 9/challenge9.sql
+++ Calendar/Day 9/challenge9.sql
@@ -1,4 +1,5 @@
 -- Este foi o desafio mais difícil que concluí, levei horas e precisei estudar conceitos novos que ainda não conhecia.
+
 WITH RankedDishes AS (
     SELECT dish_name, event_name, CAST(calories AS REAL) / weight_g AS calorie_density,
         ROW_NUMBER() OVER (PARTITION BY events.event_id ORDER BY CAST(calories AS REAL) / weight_g DESC) AS top_3
